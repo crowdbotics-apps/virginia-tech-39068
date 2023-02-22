61428d8bdb3f067809b730c4dc62fdf2f870b4e2
Crowdbotics <team@crowdbotics.com>
2023-02-22 18:21:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "virginia-tech-39068",
+  url: "https://virginia-tech-39068.botics.co",
+  api: "https://virginia-tech-39068.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
